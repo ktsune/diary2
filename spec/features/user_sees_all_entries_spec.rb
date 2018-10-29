625f384dfefd7da787f5924ae7c17e5fb6a8960f
Sarah Tatro <statro09@gmail..com>
2018-10-29 15:07:58 -0600

    Set up /entries route and action

--- spec/features/user_sees_all_entries_spec.rb
+++ spec/features/user_sees_all_entries_spec.rb
@@ -1,17 +1,17 @@
 require "rails_helper"
 
-describe "user sees all articles" do
-  describe "they visit /articles" do
-    it "displays all articles" do
+describe "user sees all entries" do
+  describe "they visit /entries" do
+    it "displays all entries" do
 
       entry_1 = Entry.create!(title: "Title 1", body: "Body 1")
       entry_2 = Entry.create!(title: "Title 2", body: "Body 2")
 
-      visit '/articles'
+      visit '/entries'
 
-      expect(page).to have_content(article_1.title)
-      expect(page).to have_content(article_2.title)
-      
+      expect(page).to have_content(entry_1.title)
+      expect(page).to have_content(entry_2.title)
+
     end
   end
 end
